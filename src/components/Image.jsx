cc9d267f8e3643fec44bbcd5e51619f32b515113
Clinton wambugu <clintonwambugu@gmail.com>
2022-12-27 16:28:05 +0300

    Add cart and heart icon

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
 function Image({photo, className}) {
+  function heartIcon() {
+    return <i className="ri-heart-line favorite" ></i>
+  }
+
+  function cartIcon() {
+    return <i className="ri-add-circle-line cart"></i>
+  }
+
   return (
     <div className={`${className} image-container`}>
         <img src={photo.url} className="image-grid"/>
+        {heartIcon()}
+        {cartIcon()}
     </div>
   )
 }
